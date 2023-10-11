6f6759ab023f06926f7c19b3ede112b1875b7b98
Pike <nghiavt1@arrowhitech.co>
2023-10-11 18:17:32 +0700

    make admin login UI

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Admin Login</title>
-</head>
-<body>
-<form method="POST" action="{{ route('admin.login') }}">
-    @csrf
-    <h1>Admin</h1>
-    <input type="text" name="email" placeholder="Nhập địa chỉ email">
-    <input type="password" name="password" placeholder="Nhập mật khẩu">
-    <button type="submit">Đăng nhập</button>
-</form>
-</body>
-</html>
+@extends('admin.layouts.app')
+@section('title', 'Admin Login')
+<div class="admin-login">
+    <img src="{{ asset('/images/admin/admin-bg.jpg') }}" alt="">
+    <div class="admin-login-form">
+        <form method="POST" action="{{ route('admin.login') }}">
+            @csrf
+            <h1>Admin</h1>
+            <input type="text" name="email" placeholder="Nhập địa chỉ email">
+            <input type="password" name="password" placeholder="Nhập mật khẩu">
+            <button class="admin-login-btn" type="submit">
+                <span>Login</span>
+            </button>
+        </form>
+    </div>
+</div>
