2fd9ddd8584997041611a51d4bc8e2e00801b433
Pike <nghiavt1@arrowhitech.co>
2023-10-18 13:33:12 +0700

    market-page

--- app/Http/Controllers/User/MarketController.php
+++ app/Http/Controllers/User/MarketController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Merchandise;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MarketController extends Controller
 {
     public function index()
     {
-        $merchandises = Merchandise::all();
+        $merchandises = DB::table('merchandises')
+                        ->join('users', 'merchandises.user_id', '=', 'users.id')
+                        ->select('merchandises.*', 'users.avatar', 'users.name as username')->get();
 
         return view('market', ['merchandises' => $merchandises]);
     }
--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -7,7 +7,25 @@
             <div class="card">
                 <div class="card-header">{{ __('Market') }}</div>
                 <div class="card-body">
-                    
+                    @if (isset($merchandises) && count($merchandises) > 0)
+                        @foreach ($merchandises as $item)
+                            <div class="merchandise">
+                                <div class="avatar-field">
+                                    <img src="{{asset($item->avatar)}}" alt="">
+                                    <p>seller: {{$item->username}}</p>
+                                    <div class="price-box">
+                                        <span>@include('svg.pokedollars')</span>
+                                        <span class="price">{{ number_format($item->price, 0, ",", ".") }}</span>
+                                    </div>
+                                </div>
+                                <div class="merchandise-details">
+                                    <h2>{{ $item->name }}</h2>
+                                    <img src="{{$item->image}}" alt="">
+                                    <p>{{$item->description}}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
