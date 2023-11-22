3e3a6c4e5442ae23ee69aa25393b4b8ac3635643
Pike <nghiavt1@arrowhitech.co>
2023-11-22 18:52:29 +0700

    update vi lang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -185,13 +185,13 @@
                     @guest
                         @if (Route::has('login'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('messages.header.login') }}</a>
                             </li>
                         @endif
 
                         @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('messages.header.register') }}</a>
                             </li>
                         @endif
                     @else
