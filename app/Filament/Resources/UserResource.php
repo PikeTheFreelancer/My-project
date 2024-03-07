876ab214de51195a3a7ad652493ac42378b0ab68
pikeTheDev <pikefreeman1997@gmail.com>
2024-03-07 14:47:45 +0700

    update

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -33,10 +33,6 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
                 Forms\Components\TextInput::make('avatar')
                     ->maxLength(255),
                 Forms\Components\Toggle::make('status'),
